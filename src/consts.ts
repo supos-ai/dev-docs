9950648f436ee3de0b47c428f24ae3b8ee43ca53
sunzhiqi <sunzhiqi@live.com>
2023-11-17 01:38:17 +0800

    docs

--- src/consts.ts
+++ src/consts.ts
@@ -106,6 +106,6 @@ export const NAVIGATION = [
     {href: '/', title: '主页'},
     {href: '/blog', title: '文章'},
     {href: '/tags', title: '标签'},
-    {href: '/projects', title: '项目'},
+    // {href: '/projects', title: '项目'},
     {href: '/about', title: '关于'},
 ]
